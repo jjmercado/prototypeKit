551189d46845dc6e6ea1ebeb62cc1457092cbc39
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2023-09-05 11:59:25 +0200

    Add back button component

---
+++ src/lib/backButton.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let path = "/";
+</script>
+
+<div>
+    <a href={path}><button>Back</button></a>
+</div>
+
+<style>
+    div
+    {
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    button
+    {
+        height: 5rem;
+        width: 5rem;
+    }
+</style>
